3a41af8d144094696bdf5e5149bb368f0b2fe8bc
Mark-lord3 <m.s.trukhin@student.khai.edu>
2021-11-25 17:19:27 +0200

    ChatBot {1-6}stage

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,61 @@
+def hel():
+    print("Hello !My name is ChatBot")
+    print("I was created in 2021")
+
+
+hel()
+
+
+def name():
+    print("Please,remind me your name.")
+    a = input()
+    print("what a great name you have," + a)
+
+
+name()
+
+
+def ag():
+    print("Let me guess your age.")
+    print("Enter remainders of dividing your age by 3, 5 and 7.")
+    re3 = int(input())
+    re5 = int(input())
+    re7 = int(input())
+    age = (re3 * 70 + re5 * 21 + re7 * 15) % 105
+    print("Your age is" + str(age) + "; that's a good time to start programming!")
+
+
+ag()
+
+
+def digit():
+    print("Now I will prove to you that I can count to any number you want.")
+    d = int(input())
+    i = 0
+    while i < d:
+        print("!" + str(i))
+        i += 1
+    print("Completed, have a nice day!")
+
+
+digit()
+
+
+def know():
+
+    print("Let's test your programming knowledge.")
+    print("Why do we use methods?")
+    print("1. To repeat a statement multiple times.")
+    print("2. To decompose a program into several small subroutines.")
+    print("3. To determine the execution time of a program.")
+    print("4. To interrupt the execution of a program.")
+
+    while f != 4:
+        print("Please, try again.")
+        f = int(input())
+
+    print("Completed, have a nice day!")
+    print("Congratulations, have a nice day!")
+
+
+know()
