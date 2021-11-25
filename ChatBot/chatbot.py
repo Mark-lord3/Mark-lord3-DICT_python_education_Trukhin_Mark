e4b9f9e9a599df761c8b9949956030d6a55857e8
Mark-lord3 <m.s.trukhin@student.khai.edu>
2021-11-25 17:35:34 +0200

    ChatBot {5}stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -20,3 +20,16 @@ while(i<d):
 
 
 print("Completed, have a nice day!")
+print("Let's test your programming knowledge.")
+print("Why do we use methods?")
+print("1. To repeat a statement multiple times.")
+print("2. To decompose a program into several small subroutines.")
+print("3. To determine the execution time of a program.")
+print("4. To interrupt the execution of a program.")
+
+while(f!=4):
+    print("Please, try again.")
+    f=int(input())
+
+print("Completed, have a nice day!")
+print("Congratulations, have a nice day!")
